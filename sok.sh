5b1e43e0b9a1562619a5413426889e0da504bb8a
LaRocaDan <53381602+larocadan@users.noreply.github.com>
2020-04-13 09:50:56 -0300

    Update sok.sh

--- sok.sh
+++ sok.sh
@@ -17,14 +17,16 @@
 	    then
 		    Debian=$(/etc/debian_version);
 		    SO=$Debian
-		else
-			DebianX=$(lsb_release -a)
-			SO=$DebianX
-		else 
-			DebianY=$(uname -a)
-			SO=$DebianY		
-		fi
-    fi
+			else
+				DebianX=$(lsb_release -a)
+				SO=$DebianX
+				else 
+					DebianY=$(uname -a)
+					SO=$DebianY		
+				fi
+			fi    
+    	fi
+    fi  
 #identificamos el panel de control y su versión, importante para otras funciones
 	if cat /usr/local/directadmin/custombuild/versions.txt > /dev/null 2>&1
 	then
